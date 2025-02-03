8ab1ff6e80740e5b3f97404cc37b7c46859cfd07
pnolle <pnolle@web.de>
2025-02-03 22:12:35 +0100

    Artnet.parse() ok

--- src/Sample_ETH_W5500_Arduino_SPI.ino
+++ src/Sample_ETH_W5500_Arduino_SPI.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <EthernetUdp.h>
+#include <ArtnetEther.h>
 
 // Pin definitions based on your wiring
 #define SCK_PIN 14
@@ -19,8 +19,23 @@ IPAddress gateway(192, 168, 1, 5);     // Gateway IP
 IPAddress subnet(255, 255, 255, 0);    // Subnet Mask
 IPAddress primaryDNS(8, 8, 8, 8);   // optional
 
-unsigned int localPort = 8888;       // local port to listen for UDP packets
-EthernetUDP Udp;    // A UDP instance to let us send and receive packets over UDP
+unsigned int localPort = 6454; // Art-Net standard port
+ArtnetReceiver artnet;    // Art-Net instance
+uint16_t universe1 = 1;  // 0 - 15
+uint16_t universe2 = 2;  // 0 - 15
+
+void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data) {
+    Serial.print("Universe: ");
+    Serial.print(universe);
+    Serial.print(", Length: ");
+    Serial.print(length);
+    Serial.print(", Data: ");
+    for (int i = 0; i < length; i++) {
+        Serial.print(data[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+}
 
 void setup() {
     // Initialize Serial for debugging
@@ -43,17 +58,28 @@ void setup() {
     } else {
         Serial.println("Ethernet initialization failed. Check wiring and settings.");
     }
-  Udp.begin(localPort);
+    
+    // Start Art-Net
+    artnet.begin();
+    
+    // // if Artnet packet comes to this universe, this function is called
+    // artnet.subscribeArtDmxUniverse(universe1, [&](const uint8_t* data, const uint16_t size) {
+    //     Serial.print("artnet data (universe : ");
+    //     Serial.print(universe1);
+    //     Serial.print(", size = ");
+    //     Serial.print(size);
+    //     Serial.print(") :");
+    //     for (size_t i = 0; i < size; ++i) {
+    //         Serial.print(data[i]);
+    //         Serial.print(",");
+    //     }
+    //     Serial.println();
+    // });
+
+    // // you can also use pre-defined callbacks
+    // artnet.subscribeArtDmxUniverse(universe2, onDmxFrame);
 }
 
-void loop() {  
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    char packetBuffer[packetSize + 1];
-    Udp.read(packetBuffer, packetSize);
-    packetBuffer[packetSize] = '\0'; // Null-terminate the string
-    Serial.print("Received packet: ");
-    Serial.println(packetBuffer);
-  }
-  delay(10);
+void loop() {
+    artnet.parse();
 }
